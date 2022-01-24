20e207a20ed1ab98268a146febfe4bbaeae5339e
Danny van Heumen <danny@dannyvanheumen.nl>
2022-01-25 00:29:44 +0100

    Policy flags

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
 use authentication::AKEError;
+use bitflags::bitflags;
 use crypto::CryptoError;
 
 extern crate aes_ctr;
 extern crate base64;
+extern crate bitflags;
 extern crate hex;
 extern crate num;
 extern crate num_bigint;
@@ -19,7 +21,16 @@ mod protocol;
 pub mod host;
 pub mod session;
 
-/// Message represents the resulting Message intended for the messaging client, possibly containing content relevant to display to the user.
+// TODO early implementation assumptions:
+// 1. injections of messages into the transport never fails.
+// 2. OTR is always enabled. (clarify nuances of this/meaning of this)
+// 3. add message fragmentation.
+// 4. ...
+
+// TODO initialization-time checking:
+//   1. CPU capabilities: usize 32-bit or 64-bit, given checking for appropriate boundaries throughout code. (e.g. encoding.rs serialization)
+
+/// UserMessage represents the resulting Message intended for the messaging client, possibly containing content relevant to display to the user.
 pub enum UserMessage {
     /// Nothing received that is relevant to report/transfer back to the messaging client.
     None,
@@ -73,20 +84,45 @@ pub enum Version {
 /// InstanceTag represents a client instance tag. The instance tag is used to distinguish between multiple clients using the same account.
 pub type InstanceTag = u32;
 
+// FIXME: can we do without CTR_LEN const?
 /// CTR type represents the first half of the counter value used for encryption, which is transmitted between communicating parties.
-type CTR = [u8; 8];
+const CTR_LEN: usize = 8;
+type CTR = [u8; CTR_LEN];
 
 /// MAC type represents the 20-byte MAC value.
-type MAC = [u8; 20];
+const MAC_LEN: usize = 20;
+type MAC = [u8; MAC_LEN];
 
 /// Signature type represents a DSA signature in IEEE-P1363 representation.
-type Signature = [u8; 40];
+const SIGNATURE_LEN: usize = 40;
+type Signature = [u8; SIGNATURE_LEN];
 
 #[derive(Debug)]
 pub struct TLV(pub u16, pub Vec<u8>);
 
-// TODO early implementation assumptions:
-// 1. injections of messages into the transport never fails.
-// 2. OTR is always enabled.
-// 3. add message fragmentation.
-// 4. ...
+// TODO implement use of policy flags!
+bitflags! {
+    struct Policy: u32 {
+    // ALLOW_V1
+    //     Allow version 1 of the OTR protocol to be used (in general this document will not address OTR protocol version 1; see previous protocol documents for these details).
+    //const ALLOW_V1 = 0b00000001;
+    // ALLOW_V2
+    //     Allow version 2 of the OTR protocol to be used.
+    //const ALLOW_V2 = 0b00000010;
+    // ALLOW_V3
+    //     Allow version 3 of the OTR protocol to be used.
+    const ALLOW_V3 = 0b00000100;
+    // REQUIRE_ENCRYPTION
+    //     Refuse to send unencrypted messages.
+    const REQUIRE_ENCRYPTION = 0b00001000;
+    // SEND_WHITESPACE_TAG
+    //     Advertise your support of OTR using the whitespace tag.
+    const WHITESPACE_TAG = 0b00010000;
+    // WHITESPACE_START_AKE
+    //     Start the OTR AKE when you receive a whitespace tag.
+    const WHITESPACE_START_AKE = 0b00100000;
+    // ERROR_START_AKE
+    //     Start the OTR AKE when you receive an OTR Error Message.
+    const ERROR_START_AKE = 0b01000000;
+    }
+}
